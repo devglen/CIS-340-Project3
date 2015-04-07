6d19e8999be3e3791027149f432ca33eff34a6c6
Derek Morey <dman6505@gmail.com>
2015-04-07 00:41:45 -0400

    updated help

--- help.c
+++ help.c
@@ -7,7 +7,7 @@ int main(void)
     printf("    fmount <file_name>      - mount the specified image file\n");
     printf("    fumount                 - umount the mounted floppy disk. \n");
     printf("    cd <path>               - change directories. \n");
-    printf("    path [+][-] <path>      - print current path or modify the path. \n");
+    printf("    path [+][-] [path]      - print current path or modify the path. \n");
     printf("    structure               - list the structure of the floppy disk image.\n");
     printf("    traverse [-l]           - list the contents in the root directory. Optional -l flag gives a long listing of the root directory.\n");
     printf("    showsector <sector_num> - show the content of the given sector.\n");
