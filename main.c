538aed89a832f845b020d35b8f1feb79c1880996
Oisota <dman6505@gmail.com>
2015-04-07 11:41:26 -0400

    updated main

--- main.c
+++ main.c
@@ -14,6 +14,7 @@ int main(void)
     while (1) {
         printf("\n flop: ");
         char input[50], command[50], arg[50];
+        char *const args[10], *const env[10];
 
         fgets(input, 50, stdin); //read line
         sscanf(input, "%s", command); //separate out first arg, consider using strtok() instead of sscanf()
@@ -22,28 +23,28 @@ int main(void)
             printf("Exiting the floppy disk shell... \n");
             return EXIT_SUCCESS;
         } else if (strcmp("help", command) == 0) {
-            execve("./help","","");
+            execve("./help", args, env);
         } else if (strcmp("fmount", command) == 0) {
             sscanf(input, "%s %s", command, arg);
-            fmount((const char*) arg);
+            //fmount((const char*) arg);
         } else if (strcmp("fumount", command) == 0) {
-            fumount(fd);
+            //fumount(fd);
         } else if (strcmp("structure", command) == 0) {
-            execve("./structure");
+            execve("./structure", args, env);
         } else if (strcmp("traverse", command) == 0) {
             if (strstr(input, "-l") == NULL) {
-                execve("./traverse", command);
+                execve("./traverse", args, env);
             } else if (strstr(input, "-l") != NULL) {
                 sscanf(input, "%s %s", command, arg);
-                execve("./traverse", arg);
+                execve("./traverse", args, env);
             } else {
                 printf("Error invalid argument, please try again! \n");
             }
         } else if (strcmp("showsector", command) == 0) {
             sscanf(input, "%s %s", command, arg);
-            execve("./show_sector",atoi(arg));
+            execve("./show_sector", args, env);
         } else if (strcmp("showfat", command) == 0) {
-            execve("./show_fat");
+            execve("./show_fat", args, env);
         } else {
             printf("Error: invalid command\n");
         }
