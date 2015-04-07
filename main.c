f3f7816b06e310f915fcbc13bf4b9998a526953e
Oisota <dman6505@gmail.com>
2015-04-07 15:05:30 -0400

    cleaned up syntax

--- main.c
+++ main.c
@@ -12,26 +12,26 @@
 int main(void)
 {
     while (1) {
-        printf("\n flop: ");
+        printf("flop: ");
         char input[50], command[50], arg[50];
         char *const args[10], *const env[10];
 
         fgets(input, 50, stdin); //read line
         sscanf(input, "%s", command); //separate out first arg, consider using strtok() instead of sscanf()
 
-        if (strcmp("exit", command) == 0) {
+        if (!strcmp("exit", command)) {
             printf("Exiting the floppy disk shell... \n");
             return EXIT_SUCCESS;
-        } else if (strcmp("help", command) == 0) {
+        } else if (!strcmp("help", command)) {
             execve("./help", args, env);
-        } else if (strcmp("fmount", command) == 0) {
+        } else if (!strcmp("fmount", command)) {
             sscanf(input, "%s %s", command, arg);
             //fmount((const char*) arg);
-        } else if (strcmp("fumount", command) == 0) {
+        } else if (!strcmp("fumount", command)) {
             //fumount(fd);
-        } else if (strcmp("structure", command) == 0) {
+        } else if (!strcmp("structure", command)) {
             execve("./structure", args, env);
-        } else if (strcmp("traverse", command) == 0) {
+        } else if (!strcmp("traverse", command)) {
             if (strstr(input, "-l") == NULL) {
                 execve("./traverse", args, env);
             } else if (strstr(input, "-l") != NULL) {
@@ -40,10 +40,10 @@ int main(void)
             } else {
                 printf("Error invalid argument, please try again! \n");
             }
-        } else if (strcmp("showsector", command) == 0) {
+        } else if (!strcmp("showsector", command)) {
             sscanf(input, "%s %s", command, arg);
             execve("./show_sector", args, env);
-        } else if (strcmp("showfat", command) == 0) {
+        } else if (!strcmp("showfat", command)) {
             execve("./show_fat", args, env);
         } else {
             printf("Error: invalid command\n");
