9efe93241ff266a391379967f87ed006d70be597
rtrebar <btrebar@gmail.com>
2015-04-08 11:43:38 -0400

    Added support for path methods

--- main.c
+++ main.c
@@ -12,8 +12,8 @@
 int main(void)
 {
     while (1) {
-        printf("flop: ");
-        char input[50], command[50], arg[50];
+        printf("\nflop: ");
+        char input[50], command[50], arg[50], mod[50];
         char *const args[10], *const env[10];
 
         fgets(input, 50, stdin); //read line
@@ -22,7 +22,30 @@ int main(void)
         if (!strcmp("exit", command)) {
             printf("Exiting the floppy disk shell... \n");
             return EXIT_SUCCESS;
-        } else if (!strcmp("help", command)) {
+        } else if (!strcmp("path", command)) {
+		    sscanf(input, "%s %s %s", command, arg, mod);
+	  	    if ((strchr(arg,'+') == NULL) && (strchr(arg,'-') == NULL)) {
+                path();
+            } else if (!strcmp("+", arg)) {
+		        if(strchr(mod,'#') == NULL){
+                    pathadd((const char*)mod);
+		            strcpy(arg,"");
+		        }
+	        	else{
+		            printf("Error invalid argument, please try again! \n");
+                }
+            }else if (!strcmp("-", arg)) {
+		        if(strchr(mod,'#') == NULL){
+                    pathsub((const char*)mod);
+		            strcpy(arg,"");
+		        }
+		    else{
+		        printf("Error invalid argument, please try again! \n");
+            }
+        } else{
+		    printf("Error invalid argument, please try again! \n");
+	    }
+	}else if (!strcmp("help", command)) {
             execve("./help", args, env);
         } else if (!strcmp("fmount", command)) {
             sscanf(input, "%s %s", command, arg);
