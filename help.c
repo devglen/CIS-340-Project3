733e843c0aa9bf6ac6576be50ce0c652ce0fa506
devglen <devglen@icloud.com>
2015-04-12 13:37:08 -0400

    cleanup: removed fmount and fumount as unnecessary also added new print type

--- help.c
+++ help.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 
-int main(void)
+int main()
 {
-    printf("The available commands are:\n");
-    printf("    help                    - display a list of available commands.\n");
-    printf("    fmount <file_name>      - mount the specified image file\n");
-    printf("    fumount                 - umount the mounted floppy disk. \n");
-    printf("    cd <path>               - change directories. \n");
-    printf("    path [+][-] [path]      - print current path or modify the path. \n");
-    printf("    structure               - list the structure of the floppy disk image.\n");
-    printf("    traverse [-l]           - list the contents in the root directory. Optional -l flag gives a long listing of the root directory.\n");
-    printf("    showsector <sector_num> - show the content of the given sector.\n");
-    printf("    showfat                 - show the content of the FAT table.\n");
-    printf("    exit                    - exit the floppy shell \n");
+    fprintf(stdout, "The available commands are:\n");
+    fprintf(stdout, "    help                    - display a list of available commands.\n");
+    fprintf(stdout, "    cd <path>               - change directories. \n");
+    fprintf(stdout, "    path [+][-] [path]      - print current path or modify the path. \n");
+    fprintf(stdout, "    structure               - list the structure of the floppy disk image.\n");
+    fprintf(stdout, "    traverse [-l]           - list the contents in the root directory. Optional -l flag gives a long listing of the root directory.\n");
+    fprintf(stdout, "    showsector <sector_num> - show the content of the given sector.\n");
+    fprintf(stdout, "    showfat                 - show the content of the FAT table.\n");
+    fprintf(stdout, "    exit                    - exit the floppy shell \n");
     return 0;
 }
