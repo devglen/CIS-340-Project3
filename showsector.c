ffe2a2eab0b9008901b9dad604d49bb1352adabe
devglen <devglen@icloud.com>
2015-04-12 13:39:57 -0400

    udated showsector method

--- showsector.c
+++ showsector.c
@@ -1,5 +1,82 @@
-int main(int argc, const char *argv[])
-{
-    
-    return 0;
+/*
+ * showsector.c
+ *
+ *  Created on: Apr 5, 2015
+ *      Author: Glen
+ */
+
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int fd;
+
+int main(int argc, char **argv) {
+	unsigned short bytes_per_sector;
+	unsigned short num_of_sectors;
+
+	if ((fd = open("/Users/Glen/Downloads/imagefile.img", O_RDONLY)) < 0) {
+		printf("There was an error mounting floppy\n");
+		exit(1);
+	}
+
+	unsigned short low, high;
+	char buf[32];
+	size_t number_of_bytes;
+	ssize_t bytes_read;
+	number_of_bytes = sizeof(buf);
+	off_t offset;
+
+	offset = lseek(fd, SEEK_SET, SEEK_SET);
+	if (offset == -1) {
+		printf("There was an issue reading beginning of floppy.");
+		exit(1);
+	}
+
+	bytes_read = read(fd, buf, number_of_bytes);
+	if (bytes_read != number_of_bytes) {
+		printf("There was an issue reading the floppy.");
+		exit(1);
+	}
+
+	low = ((unsigned short) buf[11]) & 0xff;
+	high = ((unsigned short) buf[12]) & 0xff;
+	bytes_per_sector = low | (high << 8);
+
+	low = ((unsigned short) buf[22]) & 0xff;
+	high = ((unsigned short) buf[23]) & 0xff;
+	num_of_sectors = low | (high << 8);
+
+	offset = lseek(fd, bytes_per_sector, 0);
+	if (offset != bytes_per_sector) {
+		printf("There was an issue setting the cursor");
+		exit(1);
+	}
+
+	int i;
+	unsigned short sec;
+	unsigned char hex[bytes_per_sector];
+	sec = atoi(*argv);
+	printf("\nhex dump of sector : %d", sec);
+
+	// set up horizontal entry hex values
+	printf("\n");
+	printf("\t 0 \t 1 \t 2 \t 3 \t 4 \t 5 \t 6 \t 7 \t 8 \t 9 \t A \t B \t C \t D \t E \t F");
+
+	lseek(fd,sec*bytes_per_sector,SEEK_SET);
+	for(i=0;i<bytes_per_sector;i++) {
+		read(fd,&hex[i],1);
+		if(i % 16 == 0){
+			printf("\n %x ",i);
+		}
+		printf("\t %x ", hex[i]);
+	}
+	printf("\n");
+	close(fd);
+	return 0;
 }
