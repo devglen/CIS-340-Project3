54053593dd977e4dc69a16ecaf6b572173af6abb
devglen <devglen@icloud.com>
2015-04-12 13:41:26 -0400

    minor changes to traverse will add logic once completed locally

--- traverse.c
+++ traverse.c
@@ -1,5 +1,14 @@
-int main(int argc, const char *argv[])
-{
-    
-    return 0;
+#include <stdio.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(int argc, char **argv) {
+	// TODO: add traverse logic
+	return 0;
 }
